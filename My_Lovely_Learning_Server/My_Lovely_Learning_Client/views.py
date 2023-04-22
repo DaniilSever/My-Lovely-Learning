5988ad64302e1ccf6dece8faa63727241da2373a
Denver <121034377+DaniilSever@users.noreply.github.com>
2023-04-22 10:40:57 +0700

    10:40 22.04

--- My_Lovely_Learning_Server/My_Lovely_Learning_Client/views.py
+++ My_Lovely_Learning_Server/My_Lovely_Learning_Client/views.py
@@ -45,6 +45,15 @@ class LoginUser(LoginView):
 def my_cource(request):
     return render(request, 'My_Lovely_Learning_templates/user_learn_cource.html')
 
+def favorit_cource(request):
+    return render(request, 'My_Lovely_Learning_templates/user_learn_cource_favorit.html')
+
+def archiv_cource(request):
+    return render(request, 'My_Lovely_Learning_templates/user_learn_cource_archiv.html')
+
+def class_cource(request):
+    return render(request, 'My_Lovely_Learning_templates/user_learn_class.html')
+
 def profile(request):
     return render(request, 'My_Lovely_Learning_templates/profile.html')
 
