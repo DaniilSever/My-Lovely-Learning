aca97a36ef554dbc5931c8059d2ab9ee0c700642
Denver <121034377+DaniilSever@users.noreply.github.com>
2023-05-10 01:21:38 +0700

    c

--- My_Lovely_Learning_Server/My_Lovely_Learning_Client/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import * 
-
-urlpatterns = [
-    path('', redirectMe),
-    path('home/', index, name='main'),
-    path('home/catalog/', catalog, name='catalog')
-]   
