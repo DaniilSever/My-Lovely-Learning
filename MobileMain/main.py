2570e3ddb185ba2764d0261297b4ee535777e2ff
Denver <121034377+DaniilSever@users.noreply.github.com>
2023-05-12 13:47:22 +0700

    12

--- MobileMain/main.py
+++ MobileMain/main.py
@@ -4,6 +4,8 @@ from kivy.config import Config
 
 # -------------------------UIX------------------------ #
 from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
 # ---------------------------------------------------- #
 
 # -----------------------Config----------------------- #
@@ -16,7 +18,27 @@ Config.write()
 # -----------------------Window----------------------- #
 class Catalog(Screen):
     Builder.load_file('./kv/catalog.kv')
-class Profile(Screen):pass
+
+    # -----------------------Widgets---------------------- #
+    class Header_Dropdown(DropDown):
+        Builder.load_file('./widgets/headerDropDown.kv')
+
+    # ---------------------------------------------------- #
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        dropdown_btn = self.ids.dropdown_profile
+        dropdown = Catalog.Header_Dropdown()
+        dropdown_btn.bind(on_release=dropdown.open)
+        dropdown.bind(on_select=lambda instance, x: setattr(dropdown_btn, 'text', x))
+        
+        Profile_btn = self.ids.profile
+        Profile_btn.bind(on_release=self.root.manager.current('profile'))
+
+
+
+class Profile(Screen):
+    Builder.load_file('./kv/profile.kv')
 # ---------------------------------------------------- #
 
 
